9f0f63a1379567498176d2ff84f43985e07f0d7b
Pedro Molina <pedro.molina@petroprix.com>
2022-09-16 19:12:52 +0200

    terminada clase 192

--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -28,14 +28,16 @@ class Pagina1Page extends StatelessWidget {
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
         final curvedAnimation =
             CurvedAnimation(parent: animation, curve: Curves.easeInOut);
-
-        return SlideTransition(
+        /* return SlideTransition(
           position: Tween<Offset>(begin: Offset(0.0, -1.0), end: Offset.zero)
               .animate(curvedAnimation),
           child: child,
-        );
-      },
-      transitionDuration: Duration(seconds: 2),
+        ); */
+        return ScaleTransition(
+            child: child,
+            scale:
+                Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation));
+      }, // transitionDuration: Duration(seconds: 2),
     );
   }
 }
